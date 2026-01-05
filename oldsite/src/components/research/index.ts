6e5e7b7cb88519826ef518293891db310c220c66
Unknown-user-C <42628079+jangchiang@users.noreply.github.com>
2026-01-05 13:40:32 +0700

    fix docker error

--- oldsite/src/components/research/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ResearchHero } from './ResearchHero';
-export { default as ResearchArea } from './ResearchArea';
-export { default as ResearchCategory } from './ResearchCategory';
-export { default as ResearchSection } from './ResearchSection';
--- oldsite/src/i18n/config.ts
+++
@@ -1,5 +0,0 @@
-// src/i18n/config.ts
-export const i18n = {
-    defaultLocale: 'en',
-    locales: ['en', 'th']
-  }
